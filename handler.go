fbfcb92beb4df29f08d1c8c3b8ceb3f69d8b8a6a
Brian Espina <espinabrian@gmail.com>
2026-07-19 17:22:09 +0800

    Added SET and GET commands
    its actually working now holy shit. this is fun.

--- handler.go
+++ handler.go
@@ -12,6 +12,7 @@ func handleConn(c net.Conn) {
 	fmt.Println("client connected", c.RemoteAddr())
 
 	r := bufio.NewReader(c)
+	store := make(map[string]string)
 
 	for {
 		args, err := readCommands(r)
@@ -38,7 +39,26 @@ func handleConn(c net.Conn) {
 
 			response := fmt.Sprintf("$%d\r\n%s\r\n", len(args[1]), args[1])
 			c.Write([]byte(response))
+		case "SET":
+			if len(args) != 3 {
+				c.Write([]byte("-ERR wrong number of arguments\r\n"))
+				break
+			}
+			store[args[1]] = args[2]
+			c.Write([]byte("+OK\r\n"))
 
+		case "GET":
+			if len(args) != 2 {
+				c.Write([]byte("-ERR wrong number of arguments\r\n"))
+				break
+			}
+			if val, ok := store[args[1]]; ok {
+				if _, err := fmt.Fprintf(c, "$%d\r\n%s\r\n", len(val), val); err != nil {
+					c.Write([]byte("-ERR writing to writer\r\n"))
+					break
+				}
+			}
+			c.Write([]byte("$-1\r\n"))
 		default:
 			c.Write([]byte("-ERR unknown command\r\n"))
 		}
